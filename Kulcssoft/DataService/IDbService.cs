a9a0c5989396dc21fb2a7641b5f7fb89e2b60ff0
Unknown <rtomilobil@gmail.com>
2018-04-28 18:45:04 +0200

    add dbservice interface

---
+++ Kulcssoft/DataService/IDbService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataService
+{
+    public interface IDbService
+    {
+        DataTable GetAll();
+
+        bool Delete(int id);
+
+        bool Insert(string name, string email);
+    }
+}
