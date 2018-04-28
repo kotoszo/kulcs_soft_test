847b4a5f4eeee9361698648f297d34f0b77c41c0
Unknown <rtomilobil@gmail.com>
2018-04-28 18:45:23 +0200

    add dataservice interface

---
+++ Kulcssoft/DataService/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService
+{
+    interface IDataService
+    {
+        bool InsertUser(string name, string email);
+        bool DeletUser(int id);
+        DataTable GetUsers();
+    }
+}
